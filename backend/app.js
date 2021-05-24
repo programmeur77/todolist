00fd1db4702b9cfde33c49b726e6439ad564506a
Benoît Puech <benoit.puech@yahoo.fr>
2021-05-24 17:16:53 +0200

    Added app.use request to count the number of Items found

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,23 @@ const dataBase = require('./includes/dbConnect');
 
 const app = express();
 
-app.use((res, req, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-
+app.use('/api/getItems', (req, res, next) => {
+    Item.countDocuments()
+    .then(item => {
+        if(item > 0) {
+            return res.status(200).json({item});
+        } else {
+            res.status(400).json({error : 'Aucun item enregistré'});
+        }
+    })
+    .catch (error => res.status(500).json({error}));
+});
 
 module.exports = app;

3099c80e6eea64fdfe99b60e98ffdb0b96ca0fbd
Benoît Puech <benoit.puech@yahoo.fr>
2021-05-24 09:19:56 +0200

    Updated to display the items found in DB

--- frontend/script.js
+++ frontend/script.js
@@ -6,6 +6,14 @@ $(document).ready(function () {
 
     var userAge = 18;
 
+    $.get("/api/getItems",
+        function (data) {
+            $.each(data, function (id, val) { 
+                $showList.append('<li>' + val.item + '</li>');
+            });
+        },
+    );
+
     $submit.click(function (e) {
         e.preventDefault();
         if ($input.val() !== '' && $input.val() !== null) {

193a5362e18b63f3aa90e321ff66b09c10b1f511
Benoît Puech <benoit.puech@yahoo.fr>
2021-05-24 11:06:35 +0200

    Accepted CORS requests + added dbConnect.js to connect database from an outside JS file

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,17 @@
 const express = require('express');
 
+const Item = require('./models/item');
+const dataBase = require('./includes/dbConnect');   
+
 const app = express();
+
+app.use((res, req, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
 
 module.exports = app;
